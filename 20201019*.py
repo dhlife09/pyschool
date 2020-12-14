3d3bef01119014c852c6ffd3fcea91902df8ed31
Dohyun Park <dhlife09@gmail.com>
2020-12-14 14:53:33 +0900

    Update 20201019*.py

--- 20201019*.py
+++ 20201019*.py
@@ -10,10 +10,9 @@ print("x % y =", x%y)   #나눈 나머지
 
 
 x, y= 10, 14
-print("x > y =", x>y)
-print("x < y =", x<y)
-print("x >= y =", x>=y)
-print("x <= y =", x<=y)
-print("x == y =", x==y)
-print("x != y =", x!=y)
-
+print("x > y =", x>y)   # x가 y보다 큰가?
+print("x < y =", x<y)   # x가 y보다 작은가?
+print("x >= y =", x>=y) # x가 y보다 크거나 같은가?
+print("x <= y =", x<=y) # x가 y보다 작거나 같은가?
+print("x == y =", x==y) # x가 y와 같은가?
+print("x != y =", x!=y) # x와 y가 같지 않은가?
